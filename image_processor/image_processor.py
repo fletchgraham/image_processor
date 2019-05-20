b317954a8d5311ccac773335b9e7298bf8627d0d
fletchgraham <sfletchgraham@gmail.com>
2019-05-20 14:13:45 -0400

    write a get path function

--- image_processor/image_processor.py
+++ image_processor/image_processor.py
@@ -1,7 +1,19 @@
 """Process tifs into small jpgs"""
+import os
+
+START_MSG = 'This script processes tifs into small jpgs.'
 
 def main():
-    pass
+    print(START_MSG)
+    src = input()
+
+
+def get_path(msg):
+    """ask user for a path and validate the response"""
+    p = input(msg + ' ')
+    while not (p and os.path.exists(p)):
+        p = input("That wasn't a path. ")
+    return p
 
 
 if __name__ == '__main__':
---
+++ image_processor/image_processor_test.py
@@ -0,0 +1,5 @@
+from image_processor import get_path
+
+test = get_path('Testing, provide a path')
+
+input('yay!')
